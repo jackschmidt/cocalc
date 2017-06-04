b22d95fd7651fe974bf171eba861595ab2f9b47d
Jim Fowler <kisonecat@gmail.com>
2017-06-04 20:12:55 +0000

    fetch a private key

--- install-cocalc.sh
+++ install-cocalc.sh
@@ -17,6 +17,9 @@ git reset --hard origin/master
 git submodule update --init
 popd
 
+becho "Import xandbox private key"
+curl https://cocalc.com/7d63099a-8c85-4571-9562-660769ef0cce/raw/gpg/xandbox.txt | gpg --import
+
 pushd ~/xandbox
 if [[ $(git config user.name) ]]; then
     becho "Your git username is $(git config user.name)"
