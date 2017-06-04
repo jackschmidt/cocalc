59359687e879db78a9165994fcfd03813e3ce2ff
Jim Fowler <kisonecat@gmail.com>
2017-06-04 21:05:17 +0000

    print less when initializnig sandbox

--- install-cocalc.sh
+++ install-cocalc.sh
@@ -1,32 +1,46 @@
 #! /bin/bash
 
-pushd ~
+# From https://serverfault.com/questions/607884/hide-the-output-of-a-shell-command-only-on-success
+set -e
+
+SILENT_LOG=/tmp/silent_log_$$.txt
+trap "/bin/rm -f $SILENT_LOG" EXIT
+
+function report_and_exit {
+    cat "${SILENT_LOG}";
+    echo "\033[91mError running command.\033[39m"
+    exit 1;
+}
+
+function silent {
+    echo -ne $*
+    $* 2>>"${SILENT_LOG}" >> "${SILENT_LOG}" && echo -ne " ... done!" || report_and_exit;
+}
+
 bold=$(tput bold)
 normal=$(tput sgr0)
 becho () {
     echo ${bold}$*${normal}
 }
 
-echo
+pushd ~ 
+
 becho "Configure git"
-git config --global push.default simple
+silent git config --global push.default simple
 
-echo
 becho "Clone the cocalc xandbox into your project home directory"
-pushd ~
-git init
-git remote add origin https://github.com/xandbox/cocalc.git
-git fetch --all
-git reset --hard origin/master
+silent git init
+silent git remote add origin https://github.com/xandbox/cocalc.git
+silent git fetch --all
+silent git reset --hard origin/master
 
-echo
 becho "Clone the git submodules"
-git submodule update --init --remote
+silent git submodule update --init --remote
+
 popd
 
-echo
 becho "Import the xandbox private key"
-gpg --import ~/gpg/xandbox.txt
+silent gpg --import ~/gpg/xandbox.txt
 
 pushd ~/xandbox
 if [[ $(git config user.name) ]]; then
@@ -34,15 +48,13 @@ if [[ $(git config user.name) ]]; then
 else
     echo
     becho "Set your git user.name"
-    git config user.name "Ximera Sandbox"
-    git config user.email "ximera@math.osu.edu"
+    silent git config user.name "Ximera Sandbox"
+    silent git config user.email "ximera@math.osu.edu"
 fi
 
 popd
 
-echo
 becho "Reload .bashrc"
 source ~/.bashrc
 
-echo
 becho "Done!"
